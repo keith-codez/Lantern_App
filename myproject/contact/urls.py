40b9a4544d4e012793d00162f4b2265104e67c11
keith <mwanazk@gmail.com>
2025-02-01 02:09:43 +0000

    yy

--- myproject/contact/urls.py
+++ myproject/contact/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('success/', views.success_view, name='success_view'),  # Success page
 ]

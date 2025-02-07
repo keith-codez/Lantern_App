7980400accd4d94f8b695a9990c7d9275269aec3
keith <mwanazk@gmail.com>
2025-02-07 10:25:37 +0000

    statis and whitenoise added

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,4 +133,9 @@ STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')  # The absolute path to the
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}

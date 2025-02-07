c8b82a523ce75a84bdfb54ed9e310141befa6484
keith <mwanazk@gmail.com>
2025-02-07 23:35:52 +0000

    changhes

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('django-insecure-y4rpr2u@wx*1da4(=x%bic6gs30&4f)#^eu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
 #'localhost', '127.0.0.1', 'lanternintelligencesolutions.com', 'lantern-production-59d8.up.railway.app
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','165.232.47.126']
 #CSRF_TRUSTED_ORIGINS = ['https://lanternintelligencesolutions.com', 'https://lantern-production-59d8.up.railway.app']
 
 # Application definition
@@ -76,17 +76,28 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':'railway',
+#         'USER':'postgres', 
+#         'PASSWORD': 'gOtMMchlzAJbRMVYOEEYmMRDHnZUCniG', 
+#         'HOST':'junction.proxy.rlwy.net',
+#         'PORT':'39761', 
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'railway',
-        'USER':'postgres', 
-        'PASSWORD': 'gOtMMchlzAJbRMVYOEEYmMRDHnZUCniG', 
-        'HOST':'junction.proxy.rlwy.net',
-        'PORT':'39761', 
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'newdb',
+        'USER': 'newuser',
+        'PASSWORD': 'newpassword',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -122,20 +133,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "contact/static"),)
- # The path to your static files
-STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')  # The absolute path to the directory where collectstatic will collect static files for deployment.
-
+STATIC_URL = 'static/'
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, "contact/static"),) must only be used when static folder is in root not app 
+# The path to your static files
+STATIC_ROOT =  BASE_DIR/'static' # The absolute path to the directory where collectstatic will collect static files for deployment.
+MEDIA_URL = "img/"
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+# STORAGES = {
+#     # ...
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
